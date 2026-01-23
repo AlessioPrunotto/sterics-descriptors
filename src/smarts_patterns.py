15744e0b4c9293d733b66591f8f0c39eb43e2e9c
Alessio Prunotto <aprunotto@ethz.ch>
2026-01-23 15:42:34 +0100

    smarts patterns for amines

---
+++ src/smarts_patterns.py
@@ -0,0 +1,14 @@
+SMARTS_PRIMARY_AMINE = (
+    "[NX3;H2;!a]"
+    # "[NX3;H2;!a;!$(NC=[!#6]);!$(NC#[!#6]);!$(NC=[!#7]);!$(NS);!$(NO);!$(NN)][#6]"
+)
+
+SMARTS_SECONDARY_AMINE = (
+    "[NX3;H1;!a]"
+    # "[NX3;H1;!a;!$(NC=[!#6]);!$(NC#[!#6]);!$(NC=[!#7]);!$(NS);!$(NO);!$(NN)][#6]"
+)
+
+SMARTS_TERTIARY_AMINE = (
+    "[NX3;H0;!a]"
+    # "[NX3;H0;!a;!$(NC=[!#6]);!$(NC#[!#6]);!$(NC=[!#7]);!$(NS);!$(NO);!$(NN)][#6]"
+)
